46c60adf78d4bc345bc7b26a32b77f5246929acc
Phil Wright <pwright@vertafore.com>
2019-03-06 09:10:38 -0700

    Create GitDemoProposalsApp solution with console app

---
+++ GitDemoProposalsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDemoProposalsApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
