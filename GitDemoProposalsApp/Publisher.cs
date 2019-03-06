e745be73a22959bc0971cd41be03da72445c4419
Phil Wright <pwright@vertafore.com>
2019-03-06 09:34:09 -0700

    Publish proposal after creating it

---
+++ GitDemoProposalsApp/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDemoProposalsApp
+{
+    public class Publisher
+    {
+        public void Publish(Proposal proposal)
+        {
+            Console.WriteLine(proposal);
+        }
+    }
+}
